e6a6815e181f63e68e68684bd3b28e3c0aa065ba
gdc <gdc@gdcs-MacBook-Pro.local>
2014-04-27 12:39:31 +0800

    Xcode 5.1 support

--- build-expat.sh
+++ build-expat.sh
@@ -21,8 +21,9 @@
 ###########################################################################
 #  Change values here													  #
 #																		  #
-VERSION="2.0.1"													      #
-SDKVERSION="5.0"														  #
+VERSION="2.1.0"                                                           #
+SDKVERSION="7.1"                                                          #
+MIN_VERSION="7.0"                                                         #
 #																		  #
 ###########################################################################
 #																		  #
@@ -32,13 +33,13 @@ SDKVERSION="5.0"														  #
 
 
 CURRENTPATH=`pwd`
-ARCHS="i386 armv6 armv7"
+ARCHS="i386 x86_64 armv7 armv7s arm64"
 DEVELOPER=`xcode-select -print-path`
 
 set -e
 if [ ! -e expat-${VERSION}.tar.gz ]; then
 	echo "Downloading expat-${VERSION}.tar.gz"
-    curl -O http://ncu.dl.sourceforge.net/project/expat/expat/2.0.1/expat-${VERSION}.tar.gz
+    curl -O http://ncu.dl.sourceforge.net/project/expat/expat/${VERSION}/expat-${VERSION}.tar.gz
 else
 	echo "Using expat-${VERSION}.tar.gz"
 fi
@@ -52,7 +53,7 @@ do
 	tar zxf expat-${VERSION}.tar.gz -C "${CURRENTPATH}/src"
 	cd "${CURRENTPATH}/src/expat-${VERSION}"
 
-	if [ "${ARCH}" == "i386" ];
+	if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]];
 	then
 		PLATFORM="iPhoneSimulator"
 	else
@@ -65,25 +66,35 @@ do
 	export DEVROOT="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer"
 	export SDKROOT="${DEVROOT}/SDKs/${PLATFORM}${SDKVERSION}.sdk"
 
-	export CC=${DEVROOT}/usr/bin/gcc
-	export LD=${DEVROOT}/usr/bin/ld
-	export CPP=${DEVROOT}/usr/bin/llvm-cpp-4.2
-	export CXX=${DEVROOT}/usr/bin/g++
+    export LD=${DEVROOT}/usr/bin/ld
+	export CC=${DEVELOPER}/usr/bin/gcc
+    export CXX=${DEVELOPER}/usr/bin/g++
+
 	unset AR
 	unset AS
-	export NM=${DEVROOT}/usr/bin/nm
-	export CXXCPP=$DEVROOT/usr/bin/llvm-cpp-4.2
-	export RANLIB=$DEVROOT/usr/bin/ranlib
-	export LDFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -L${CURRENTPATH}/lib"
-	export CFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${CURRENTPATH}/include"
-	export CXXFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${CURRENTPATH}/include"
+
+    if [[ "${ARCH}" == "i386" || "${ARCH}" == "x86_64" ]];
+    then
+        export NM=${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin/nm
+        export RANLIB=${DEVELOPER}/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib
+    else
+        export NM=${DEVROOT}/usr/bin/nm
+        export RANLIB=${DEVROOT}/usr/bin/ranlib
+    fi
+
+    export LDFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -L${CURRENTPATH}/lib -miphoneos-version-min=${MIN_VERSION}"
+	export CFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${CURRENTPATH}/include -miphoneos-version-min=${MIN_VERSION}"
+	export CXXFLAGS="-arch ${ARCH} -pipe -no-cpp-precomp -isysroot ${SDKROOT} -I${CURRENTPATH}/include -miphoneos-version-min=${MIN_VERSION}"
 
 	mkdir -p "${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk"
 	LOG="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk/build-expat-${VERSION}.log"
 
+    echo "Configure..."
 	./configure --prefix="${CURRENTPATH}/bin/${PLATFORM}${SDKVERSION}-${ARCH}.sdk" --host="${ARCH}-apple-darwin" --enable-static > "${LOG}" 2>&1
-	make >> "${LOG}" 2>&1
-	make install >> "${LOG}" 2>&1
+    echo "Make..."
+    make >> "${LOG}" 2>&1
+    echo "Make install..."
+    make install >> "${LOG}" 2>&1
 	cd "${CURRENTPATH}"
 	rm -rf "${CURRENTPATH}/src/expat-${VERSION}"
 done
